d7601747f8fcf30fb3abbd08a6bd814fc0ff3079
Heuristic <codemonkey2468@hotmail.com>
2021-01-01 12:50:26 -0500

    initial commit

---
+++ Chapter1/demo/review.py
@@ -0,0 +1,26 @@
+#chapter 1 demo 
+print(2+3*4) #14
+
+print((2+3)*4) #20
+
+print(7//3) #2
+
+print(7%3) #1
+
+print(2**100)
+
+
+print(5==10)
+
+print(10 > 9)
+
+print(list(range(10)))
+
+print(list(range(10, 1, -1)))
+
+my_name = 'David'
+print(my_name.upper())
+
+print(my_name.center(20))
+
+print(my_name.split('v'))
