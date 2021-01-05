36a10099e40c3b872b0ec8516d323dd933a1f431
Heuristic <codemonkey2468@hotmail.com>
2021-01-05 12:49:02 -0500

    debug

--- chapter3/par_checker.py
+++ chapter3/par_checker.py
@@ -2,22 +2,25 @@
 
 from pythonds.basic.stack import Stack #import the Stack class as previously defined
 
-def par_checker(symbol_string):
+def par_checker(symbolstring):
     s = Stack()
     balanced = True
     index = 0
 
-    while index < len(symbol_string) and balanced:
-        symbol = symbol_string[index]
+    while index < len(symbolstring) and balanced:
+        symbol = symbolstring[index]
         if symbol == "(":
             s.push(symbol)
 
         else:
-            s.pop()
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
 
-    index = index + 1
+        index = index + 1
 
-    if balanced and s.is_empty():
+    if balanced and s.isEmpty():
         return True
     
     else:
