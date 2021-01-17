d6f7d258f96aace64186305310487111d2ccbf4c
Heuristic <codemonkey2468@hotmail.com>
2021-01-17 12:51:59 -0500

    recursive binary search

---
+++ Chapter5/recursive_binary_search.py
@@ -0,0 +1,18 @@
+#recursive binary search
+
+def recursive_binary_search(alist, item):
+    if len(alist) == 0:
+        return False #again indentation in the book is wrong from this line below
+    else:
+        midpoint = len(alist) // 2
+        if alist[midpoint] == item:
+            return True
+        else:
+            if item < alist[midpoint]:
+                return recursive_binary_search(alist[: midpoint], item)
+            else:
+                return recursive_binary_search(alist[midpoint + 1: 1], item )
+    
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42] 
+print(recursive_binary_search(testlist, 2))
