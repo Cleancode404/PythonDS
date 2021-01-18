cc0420b2725e197db36868019e9df22b4d1f205d
Heuristic <codemonkey2468@hotmail.com>
2021-01-18 10:51:45 -0500

    bubble sort

---
+++ Chapter5/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(alist):
+    #loop through the list from left to right 
+    for pass_num in range(len(alist)-1 , 0, -1):  
+        for i in range(pass_num):
+            if alist[i] > alist[i + 1]:
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+print(bubble_sort(alist))
