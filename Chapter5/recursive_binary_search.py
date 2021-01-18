f4a03d9122c0da64383b228ede1264c2f044a06e
Heuristic <codemonkey2468@hotmail.com>
2021-01-18 08:28:24 -0500

    will not work if dataset is not in order

--- Chapter5/recursive_binary_search.py
+++ Chapter5/recursive_binary_search.py
@@ -1,5 +1,5 @@
 #recursive binary search
-
+#will not work if dataset is not in order
 def recursive_binary_search(alist, item):
     if len(alist) == 0:
         return False #again indentation in the book is wrong from this line below
@@ -14,5 +14,5 @@ def recursive_binary_search(alist, item):
                 return recursive_binary_search(alist[midpoint + 1: 1], item )
     
 
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42] 
+testlist = [0, 2, 32, 8, 13, 17, 19, 32, 42] 
 print(recursive_binary_search(testlist, 2))
